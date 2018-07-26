ae4ca37c0b3dd3719c34b0b4df6131f5cd6c1afe
Cesar Gonzalez <cesargonzalezmur@gmail.com>
2018-07-26 10:49:55 +1000

    delete unnecessary code

--- routes/apiC.js
+++ routes/apiC.js
@@ -2,20 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Customer = require('../models/customer');
 
-//Is an Empty Object
-function isEmptyObject(obj) {
-  return !Object.keys(obj).length;
-}
-//get customers
+//get customers copy
 router.get('/customers', function(req, res, next) {
   if(req.query.copy === 'true') {
     console.log('copy');
     Customer.find({}).then(function(customers) {
       res.status(200).send(customers);
-    });/*
-    Customer.find(req.query).then(function(customers) {
-      res.status(200).send(customers);
-    })﻿;*/
+    });
   } else {
     res.status(400).send('Bad Request');
   }
